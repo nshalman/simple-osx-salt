519cfb4627117a5426db70e9b50fb2541ccdd272
Nahum Shalman <nshalman-github@elys.com>
2016-02-17 16:29:38 -0500

    Make minion amenable to imaging

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ $(INSTALLER): setup-minion.sh salt.zip
 	cat $^ > $@
 	chmod +x $@
 
-salt.zip: build-salt.sh com.saltstack.salt.minion.plist
+salt.zip: build-salt.sh com.saltstack.salt.minion.plist start-salt-minion.sh
 	sudo -H ./build-salt.sh
 
 .PHONY:	clean test
@@ -13,5 +13,6 @@ clean:
 	rm -f $(INSTALLER) salt.zip
 
 test: $(INSTALLER)
+	sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
 	sudo rm -rf /usr/local/salt
 	sudo ./osx-salt-virtualenv-installer $(MASTER)
--- setup-minion.sh
+++ setup-minion.sh
@@ -2,16 +2,15 @@
 
 [[ -z ${1} ]] && echo "Please provide name of salt master as an argument" && exit 1
 
-SERIAL_NUMBER=$(python -c 'import plistlib,subprocess; print plistlib.readPlistFromString(subprocess.check_output("system_profiler SPHardwareDataType -xml",shell=True))[0]["_items"][0]["serial_number"]')
+THIS=$(cd $(dirname ${0}); pwd)/$(basename ${0})
+
+[[ $USER == root ]] || exec sudo -H ${THIS} "$@"
 
 mkdir -p /etc/salt
 echo "master: ${1}" > /etc/salt/minion
-echo "id: mac-${SERIAL_NUMBER}" >> /etc/salt/minion
 
-HERE=$(cd $(dirname ${0}); pwd)
-THIS=$(basename ${0})
 cd /
-unzip ${HERE}/${THIS}
+unzip ${THIS} &>/dev/null
 cp /usr/local/salt/lib/com.saltstack.salt.minion.plist /Library/LaunchDaemons/
 launchctl load "/Library/LaunchDaemons/com.saltstack.salt.minion.plist"
 exit $?
